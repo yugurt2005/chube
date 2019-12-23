a5a58c8178290fff6625d05c0128695075ef28be
soap <queazzyy@gmail.com>
2019-12-22 22:32:26 -0800

    "Build mode" button, isometric tilemap, basic code
    - Added a button that toggles the "build mode"
    - Added an isometric tilemap EVEN THOUGH:
         - We prolly won't use it: You can't edit the tilemap
            and do snapping things/build with it. INSTEAD:
    - Instead I'm making the Chube able to build and instantiate new parts & to prevent a bajillion gameObjects we can merge them after creation??? Idk it's hard to explain why tilemaps don't work and why this is better unless you go in the code yourself lol

---
+++ Chube/Assets/Scripts/Chube.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Chube : MonoBehaviour
+{
+    public Button buildModeButton;
+    private bool buildMode = false;
+
+    // start is called before the first frame update
+    void Start()
+    {
+        buildModeButton.onClick.AddListener(checkButton);
+    }
+
+    // update is called once per frame
+    void Update()
+    {
+        if (buildMode) build();
+    }
+
+    void build()
+    {
+        // disable all non-building related gameObjects
+        // have an isometric diamond follow your cursor and if it's aligned/snapped with another built thing/chube,
+        // then if you click it instantiates a structure
+    }
+
+    void checkButton()
+    {
+        buildMode = !buildMode;
+        Debug.Log("Button pressed and buildmode is " + buildMode);
+    }
+}

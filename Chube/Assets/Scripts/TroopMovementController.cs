0b43b1e7eaaddf687efa68af33160a6ba6ab6ad2
Chien-Yu Xiong <s-xiongch@bsd405.org>
2020-01-12 01:19:27 -0800

    Pathfinding
    something that doesn't crash. making a* work still. need to get accurate origin and dest data

--- Chube/Assets/Scripts/TroopMovementController.cs
+++ Chube/Assets/Scripts/TroopMovementController.cs
@@ -1,4 +1,5 @@
-﻿using UnityEngine;
+﻿using System.Collections.Generic;
+using UnityEngine;
 using UnityEngine.Tilemaps;
 
 public class TroopMovementController : MonoBehaviour
@@ -9,24 +10,29 @@ public class TroopMovementController : MonoBehaviour
     Rigidbody2D rbody;
     IsometricCharacterRenderer isoRenderer;
 
+    List<Vector3Int> cartesianPath = new List<Vector3Int>();
+
     void Awake()
     {
         rbody = GetComponent<Rigidbody2D>();
         isoRenderer = GetComponentInChildren<IsometricCharacterRenderer>();
     }
 
-    void FixedUpdate()
+    void Update()
     {
         if (Input.GetMouseButtonUp(0))
         {
             Vector3Int destination = tilemap.WorldToCell(Camera.main.ScreenToWorldPoint(Input.mousePosition));
             Vector3Int origin = tilemap.WorldToCell(transform.position);
 
-            Pathfinder pathfinder = new Pathfinder(null, destination, origin);
-<<<<<<< HEAD
-            
-=======
->>>>>>> troops
+            Pathfinder pathfinder = new Pathfinder(tilemap, destination, origin);
+            cartesianPath = pathfinder.path;
+        }
+
+        if (cartesianPath.Count > 0)
+        {
+            rbody.position = new Vector2((2 * cartesianPath[0].y + cartesianPath[0].x) / 2, 
+                (2 * cartesianPath[0].y - cartesianPath[0].x) / 2);
         }
     }
 }

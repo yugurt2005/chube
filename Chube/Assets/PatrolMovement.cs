717536752ef6d77be85d4ca8848cda51998e74ee
Raza Khan <razaphonelgk10@gmail.com>
2020-02-20 20:36:54 -0800

    Finished random enemy troop movement

---
+++ Chube/Assets/PatrolMovement.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PatrolMovement : MonoBehaviour
+{
+    public float speed;
+
+    public float waitTime;
+    private float startWaitTime = 3.95f;
+
+    public Transform[] moveSpots;
+    private int randomSpot;
+
+    void Start()
+    {
+        // waitTime = startWaitTime;
+        randomSpot = Random.Range(0, moveSpots.Length);
+    }
+
+    void Update()
+    {
+        startWaitTime =  Random.Range(1, 7);
+        transform.position = Vector2.MoveTowards(transform.position, moveSpots[randomSpot].position, speed * Time.deltaTime);
+
+        if (Vector2.Distance(transform.position, moveSpots[randomSpot].position) < 0.2f)
+        {
+            if (waitTime <= 0)
+            {
+                randomSpot = Random.Range(0, moveSpots.Length);
+                waitTime = startWaitTime;
+            }
+            else
+            {
+                waitTime -= Time.deltaTime;
+            }
+        }
+    }
+}
--- Chube/Assets/RandomMovement.cs
+++ Chube/Assets/RandomMovement.cs
@@ -1,18 +1,53 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AI;
 
 public class RandomMovement : MonoBehaviour
 {
+    public float timer;
+    public int newTarget;
+    public float speed;
+    public NavMeshAgent nav;
+    public Vector3 target;
+
+
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        nav = gameObject.GetComponent<NavMeshAgent>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        timer += Time.deltaTime;
+
+        if (timer >= newTarget)
+        {
+            transform.position += new Vector3(speed * Time.deltaTime, 0, 0);
+            updateSpeed();
+            newTargetMethod();
+            timer = 0;
+        }
+    }
+
+    void updateSpeed()
+    {
+        speed = Random.Range(-5, 6);
+    }
+
+    void newTargetMethod()
+    {
+        float myX = gameObject.transform.position.x;
+        float myZ = gameObject.transform.position.z;
+
+        float xPos = myX + Random.Range(myX - 100, myX + 100);
+        float zPos = myZ + Random.Range(myZ - 100, myZ + 100);
+
+        target = new Vector3(xPos, gameObject.transform.position.y, zPos);
+
+        nav.SetDestination(target);
     }
 }

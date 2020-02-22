2cf4942f7cbb0e0e1570057a60b72d1cdcc67062
Raza Khan <razaphonelgk10@gmail.com>
2020-02-21 18:41:58 -0800

    Archer enemy now spawns (50% chance). Does not do anything differently yet tho

--- Chube/Assets/Scripts/Enemies/EnemySpawner.cs
+++ Chube/Assets/Scripts/Enemies/EnemySpawner.cs
@@ -1,15 +1,19 @@
-﻿using UnityEngine;
+using UnityEngine;
 using UnityEngine.Tilemaps;
 
 public class EnemySpawner : MonoBehaviour
 {
     public GameObject prefabEnemy;
+    public GameObject prefabArcherEnemy;
     private float countdown = 5f;
     public float spawnSpeed = 5f;
 
     public Tilemap tilemap;
     public TilemapRenderer tilemapRenderer;
     public DebrisMovement edgeSpawner;
+
+
+    public int enemyTypeProbability;
 
     void Update()
     {
@@ -18,9 +22,21 @@ public class EnemySpawner : MonoBehaviour
             // uses debris's random perimeter position generator function
             Vector3 spawnPos = edgeSpawner.getPositionOnPerimeter();
 
-            GameObject enemyObj = (GameObject)Instantiate(prefabEnemy, spawnPos, transform.rotation);    
-            Enemy enemy = enemyObj.GetComponent<Enemy>();
-            enemy.onInstantiate(tilemap, tilemapRenderer);
+            enemyTypeProbability = Random.Range(0, 100);
+            if (enemyTypeProbability >= 0 & enemyTypeProbability < 50)
+            {
+                GameObject enemyObj = (GameObject)Instantiate(prefabEnemy, spawnPos, transform.rotation);
+                Enemy enemy = enemyObj.GetComponent<Enemy>();
+                enemy.onInstantiate(tilemap, tilemapRenderer); //j
+            }
+
+            if (enemyTypeProbability >= 50 & enemyTypeProbability <= 100)
+            {
+                GameObject archerEnemyObj = (GameObject)Instantiate(prefabArcherEnemy, spawnPos, transform.rotation);
+                Enemy archerEnemy = archerEnemyObj.GetComponent<Enemy>();
+                archerEnemy.onInstantiate(tilemap, tilemapRenderer);
+            }
+
             countdown = spawnSpeed;
         }
         countdown -= Time.deltaTime;

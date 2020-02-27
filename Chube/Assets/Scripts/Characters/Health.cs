1aa3d3e2987a9f8959cbabb44f8e2e2ed5abc744
soap <queazzyy@gmail.com>
2020-02-26 22:38:26 -0800

    Organized everything

---
+++ Chube/Assets/Scripts/Characters/Health.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Health : MonoBehaviour
+{
+    public float health = 100f;
+
+    public void CheckForDeath()
+    {
+        if (health <= 0)
+        {
+            Debug.Log(gameObject.tag);
+            Destroy(gameObject);   
+        }
+    }
+}
